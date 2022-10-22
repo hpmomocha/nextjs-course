5e0848ace74f9f340d83bd9119453ebbcf6cbc60
Jun Zhang <jun.zhang4@hpe.com>
2022-10-22 16:04:28 +0800

    first commit

---
+++ pages/events/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import { useRouter } from 'next/router'
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/EventsSearch'
+
+export default function AllEventsPage() {
+
+  const events = getAllEvents()
+  const router = useRouter()
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  )
+}
